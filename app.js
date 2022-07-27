a0461d111661f1d9ba241bd0229339049e52c11e
Lubomir Vaclavik <vaclaviklubomir@gmail.com>
2022-07-27 13:02:01 +0200

    recreaed function ti easier - added all star elements to html

--- app.js
+++ app.js
@@ -1,31 +1,10 @@
-const finalNumber = document.querySelector(".number");
-const showSecond = document.querySelector(".second");
-const hideFirst = document.querySelector(".first");
-const number = document.querySelectorAll(".num");
-const submit = document.querySelectorAll(".submit");
+const submit = document.querySelector(".submit");
+const second = document.querySelector(".second");
+const first = document.querySelector(".first");
+submit.addEventListener("click", () => {
+  second.classList.toggle("active");
 
-let firstButtonClicked = false;
-let secondButtonClicked = false;
-
-function Handler() {
-  firstButtonClicked = false;
-  secondButtonClicked = false;
-  finalNumber.innerHTML = "1";
-  showSecond.style.display = "block";
-  hideFirst.style.display = "none";
-  console.log(finalNumber);
-}
-
-number.forEach((num) => {
-  num.addEventListener("click", function handleClick(event) {
-    firstButtonClicked = true;
-    if (firstButtonClicked && secondButtonClicked) Handler();
-  });
+  first.classList.toggle("disable");
+  console.log(second);
 });
-
-submit.forEach((submit) => {
-  submit.addEventListener("click", function handleClick(event) {
-    secondButtonClicked = true;
-    if (firstButtonClicked && secondButtonClicked) Handler();
-  });
-});
+const textPlaceholder = document.querySelector(".you-selected");
