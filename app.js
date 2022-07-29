8b001be786c35b9b1ed64821e631fb3f5141c233
Lubomir Vaclavik <vaclaviklubomir@gmail.com>
2022-07-29 14:30:17 +0200

    added function to prevent submit without choosing rating, now for first star

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const submit = document.querySelector(".submit");
 const second = document.querySelector(".second");
 const first = document.querySelector(".first");
-submit.addEventListener("click", () => {
-  second.classList.toggle("active");
 
-  first.classList.toggle("disable");
-  console.log(second);
+const element = document.querySelector("div button");
+
+element.addEventListener("click", function handleClick() {
+  element.classList.add("number");
+
+  if (element.classList.contains("number")) {
+    submit.addEventListener("click", () => {
+      second.classList.toggle("active");
+      first.classList.toggle("disable");
+    });
+  } else {
+    submit.addEventListener("click", () => {
+      alert("choose rating");
+    });
+  }
 });
+
+submit.addEventListener("click", () => {
+  if (element.classList.contains("number")) {
+    submit.addEventListener("click", () => {
+      second.classList.toggle("active");
+      first.classList.toggle("disable");
+    });
+  } else {
+    alert("choose rating");
+    location.reload();
+  }
+});
+
 const textPlaceholder = document.querySelector(".you-selected");
